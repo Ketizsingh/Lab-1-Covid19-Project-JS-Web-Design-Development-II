ef304834ceeac5a5a97f23b9b50901043863dcbf
Mohit Rawal <mohitrawal3131@gmail.com>
2020-10-12 00:16:00 -0400

    Fonts added, style updated, script updated

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function error_logg(message) {
+  
+  document.getElementById("error_message").style.display = 'block';
+  document.getElementById("error_message").innerHTML = message;
+
+}
+
 function titleCase(str) {
   str = str.toLowerCase().split(' ');
   for (var i = 0; i < str.length; i++) {
